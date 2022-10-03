e060c16c67bb2f0573386bb673c5fa09aec3a4cc
DevGnomino <damianopezze@gmail.com>
2022-10-03 09:14:49 +0200

    Wuerfelspiel start

---
+++ python/08_wuerfelspiel.py
@@ -0,0 +1,12 @@
+import random as rand
+
+roundCounter = 0
+
+while roundCounter < 6:
+    roundCounter += 1
+
+    userInput = input("Würfeln Sie (enter)!")
+    print("Rundenresultat:\n"
+    "Spieler:\t" + str(playerInt) + "\tComputer:\t" + str(computerInt))
+    "Spielstand:\n"
+    "Spieler:\t" + str(playerInt) + "\tComputer:\t" + str(computerInt))
