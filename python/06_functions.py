777bafed684f86d5f2fb37f9e84a4b0b2bf59f77
DevGnomino <damianopezze@gmail.com>
2022-10-03 08:32:17 +0200

    python function excercise

--- python/06_functions.py
+++ python/06_functions.py
@@ -1,5 +1,24 @@
+import random as rand
+
 # Erstelle eine Funktion die 2 Zahlen addiert
+def addition(a, b):
+    sum = a + b
+    return sum
 
 # Erstelle eine Funktion, die eine zufällige Zahl zwischen 100 und 200 zurückliefert
 
+def randNum():
+    randNum = rand.randint(100,200)
+    return randNum
+
+
 # Erstelle eine Funktion, die ein Wort aus einem String Array zurückliefert.
+def oneWord(arr):
+    randIndex = rand.randint(0, len(arr))
+    word = arr[randIndex]
+    return word
+
+
+print(addition(2,4))
+print(randNum())
+print(oneWord(["Lorem", "ipsum", "dolor", "sit", "amet", "consetetur", "sadipscing", "elitr", "sed", "diam"]))
