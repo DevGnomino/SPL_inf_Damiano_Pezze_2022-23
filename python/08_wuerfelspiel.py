2d6ef9017710d74550e24799d07ec3ce3195eaf7
DevGnomino <damianopezze@gmail.com>
2022-11-21 09:51:39 +0100

    Würfelbeispiel fertig

--- python/08_wuerfelspiel.py
+++ python/08_wuerfelspiel.py
@@ -1,12 +1,29 @@
 import random as rand
-
+playerTempInt = 0
+computerTempInt = 0
+playerInt = 0
+computerInt = 0
 roundCounter = 0
 
 while roundCounter < 6:
-    roundCounter += 1
+    roundCounter += 1   
+
+    computerTempInt = rand.randint(1,6)
+    computerInt += computerTempInt
+    print("Der Computer hat gewürfelt")
 
     userInput = input("Würfeln Sie (enter)!")
-    print("Rundenresultat:\n"
-    "Spieler:\t" + str(playerInt) + "\tComputer:\t" + str(computerInt))
-    "Spielstand:\n"
-    "Spieler:\t" + str(playerInt) + "\tComputer:\t" + str(computerInt))
+    playerTempInt = rand.randint(1,6)
+    playerInt += playerTempInt
+
+    print("Rundenresultat:\n" +
+          "Spieler:\t" + str(playerTempInt) + "\tComputer:\t" + str(computerTempInt) +
+          "\nSpielstand:\n" + "Spieler:\t" + str(playerInt) + "\tComputer:\t" + str(computerInt))
+
+print("\n-----------------------------\n")
+if playerInt < computerInt:
+    print("Der Computer hat gewonnen!")
+elif playerInt == computerInt:
+    print("Das Spiel ist unentschieden zu Ende gegangen!")
+else:
+    print("Sie haben gewonnen!")
